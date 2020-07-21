5b5fc51cac02792d955a961bce698f7bb4bf3622
franky3020 <210238hwsh@gmail.com>
2020-07-21 08:21:16 +0800

    init

---
+++ src/main/java/fcu/ms/taskRibbon/TaskRibbonApplication.java
@@ -0,0 +1,13 @@
+package fcu.ms.taskRibbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskRibbonApplication.class, args);
+	}
+
+}
