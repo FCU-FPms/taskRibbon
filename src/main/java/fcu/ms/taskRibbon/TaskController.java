b09869b14aac19de5402534c74cefb942e33128a
franky3020 <210238hwsh@gmail.com>
2020-07-27 10:32:21 +0800

    新增顯示目前在使用哪一個服務

--- src/main/java/fcu/ms/taskRibbon/TaskController.java
+++ src/main/java/fcu/ms/taskRibbon/TaskController.java
@@ -1,7 +1,11 @@
 package fcu.ms.taskRibbon;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,17 +14,26 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 import reactor.core.publisher.Mono;
 
 @RestController
-
 public class TaskController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskController.class);
+
     @Autowired
     private WebClient.Builder webClientBuilder;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @GetMapping("/tasks")
-    public void getTasks() {
-        Mono<String> mono = webClientBuilder.build().get().uri("http://MS-PROVIDER-DEVELOP/tasks")
+    public Mono<String> getTasks() {
+        return webClientBuilder.build().get().uri("http://MS-PROVIDER-DEVELOP/tasks")
                 .retrieve().bodyToMono(String.class);
-        System.out.println("Franky-test");
-        mono.subscribe(System.out::println);
+
+    }
+
+    @GetMapping("/log-task-instance")
+    public void logTaskInstance() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("MS-PROVIDER-DEVELOP");
+        TaskController.LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
     }
 
 }
